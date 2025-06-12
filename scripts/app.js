ebca61cd6c6b78446d33c6d09c66a460a03e4936
May-taung <m_aung220422@fanshaweonline.ca>
2025-06-12 09:12:03 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,8 @@
-const myFullName = MayThingyanAung;
-const myStudentNumber = 1214954;
+const myFullName = 'May Thingyan Aung';
+const myStudentNumber = '1214954';
+console.log (myFullName + " - " + myStudentNumber);
+
+const primaryHeadingContent = document.querySelector ("h1");
+console.log(primaryHeadingContent.innerHTML);
+
+primaryHeadingContent.classList.add("primayHeading");
